253274c2ddc3d4a7e4e3356914a398a97d25333d
dinhnhat0401 <nhatphutho@gmail.com>
2021-10-12 21:03:26 +0900

    Added dummy data

---
+++ PhotoClient-SwiftUITests/MockData/Dummy.swift
@@ -0,0 +1,24 @@
+//
+//  Dummy.swift
+//  PhotoClient-SwiftUITests
+//
+//  Created by Đinh Văn Nhật on 2021/10/12.
+//
+
+let imageJSON: [String: Any] = [
+    "id": 12345,
+    "pageURL": "https://somewhere.com/page/",
+    "imageWidth": 2000,
+    "imageHeight": 1000,
+    "previewURL": "https://somewhere.com/preview.jpg",
+    "previewWidth": 200,
+    "previewHeight": 100,
+    "webformatURL": "https://somewhere.com/image.jpg",
+    "webformatWidth": 600,
+    "webformatHeight": 300,
+    "views": 54321,
+    "downloads": 4321,
+    "likes": 321,
+    "tags": "a, b c, d ",
+    "user": "Swinject"
+]
